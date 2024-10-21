fc59789eb7b8f73754f5dd7c17198236c5aec607
Daniar Makyev <daniarmakyev10@gmail.com>
2024-10-22 02:38:13 +0600

    настроил под local

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,12 @@
 const { Sequelize } = require('sequelize');
 
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-});
-
-
-const testConnection = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
+module.exports = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT, 
     }
-};
-
-
-testConnection();
-
-module.exports = sequelize;
+);
